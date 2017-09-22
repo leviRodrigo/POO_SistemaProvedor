2b47224fff0b5b4228515c844ad756a4207dd08e
leviRodrigo <31658181+leviRodrigo@users.noreply.github.com>
2017-09-22 08:38:23 -0300

    Add files via upload

--- TestaProvedor.java
+++ TestaProvedor.java
@@ -1,32 +1,35 @@
-import static org.junit.Assert.*;
 
-import javax.swing.JOptionPane;
+
+	import static org.junit.Assert.*;
+
+	import javax.swing.JOptionPane;
 
 import org.junit.Test;
 
+	public class TestaProvedor {
+
+		@Test
+		public void cadastraCliente() {
+			SistemaCliente sistema = new SistemaCliente();
+			String nome = JOptionPane.showInputDialog("digite o nome do cliente");
+			String cidade = JOptionPane
+					.showInputDialog("digite o nome da sua cidade:");
+			int quantMega = 40;
+			String tel = JOptionPane.showInputDialog("digite seu contato:");
+			String valor = 20;
+			sistema.cadastraCliente(nome, cidade, quantMega, tel);
+
+			sistema.pesquisaCliente("Jose");
+			assertTrue(client != null);
+			assertTrue(client1 != null);
 
 
-public class TestaProvedor {
+			int valorTotal = valor * quantMega;
 
-	@Test
-	public void cadastraCliente() {
-		SistemaCliente sistema = new SistemaCliente();
-		
-		sistema.cadastraCliente("Jose", "jacarau", 2, "99999999");
-		sistema.cadastraCliente("Maria", "mamanguape", 3, "4444444");
-		
-		Clientes client = sistema.pesquisaCliente("Jose");
-		Clientes client1 = sistema.pesquisaCliente("Maria");
-		assertTrue(client != null);
-		assertTrue(client1!= null);
-		
-		//sistema.calculaQuantMega(2);
-		//int valor = 20;
-		//int quantMega = 4;
-		//int valorTotal = valor * quantMega;
+			assertTrue(valorTotal == 80);
+			assertEquals(80, valorTotal);
 
-		//assertTrue(valorTotal == 80);
-		//assertEquals(80, valorTotal);
+		}
 
 	}
 
