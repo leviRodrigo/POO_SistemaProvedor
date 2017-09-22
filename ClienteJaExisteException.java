341cedf8f3b5f3190ea11115990287f305b318ab
leviRodrigo <31658181+leviRodrigo@users.noreply.github.com>
2017-09-22 08:38:55 -0300

    Add files via upload

---
+++ ClienteJaExisteException.java
@@ -0,0 +1,12 @@
+
+public class ClienteJaExisteException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ClienteJaExisteException(String msg){
+		super(msg);
+	}
+}
